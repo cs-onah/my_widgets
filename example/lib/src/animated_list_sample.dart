b347e19057879a4c5fdfcc5fc58b6ed94d7e6daf
Chukwuebuka Onah <revelationjay02@gmail.com>
2023-05-18 18:16:09 +0100

    created example project

---
+++ example/lib/src/animated_list_sample.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:my_widgets/my_widgets.dart';
+
+class AnimatedListSample extends StatelessWidget {
+  const AnimatedListSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SlideAnimatedList.builder(
+        itemCount: 10,
+        itemBuilder: (_, index) {
+          return ListTile(
+            leading: const CircleAvatar(),
+            title: Text("New Widget $index"),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/src/responsiveness/responsiveness.dart
+++ lib/src/responsiveness/responsiveness.dart
@@ -1,32 +1,30 @@
 part of my_widgets;
 
-/// Switches between widgets for mobile, tablet, & desktop based on screen sizes
+/// Switches between widgets based on screen sizes
 ///
-/// Provides static functions to aid responsive UI
-class ResponsiveWidget extends StatelessWidget {
+/// Provides static methods to build UI based on screen sizes
+class ResponsiveScreen extends StatelessWidget {
   final Widget mobile;
   final Widget? tablet;
   final Widget desktop;
 
-  const ResponsiveWidget({
+  const ResponsiveScreen({
     Key? key,
     required this.mobile,
     this.tablet,
     required this.desktop,
   }) : super(key: key);
 
-  /// based on screen size
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width < 600;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1024 &&
+      MediaQuery.of(context).size.width < 1100 &&
           MediaQuery.of(context).size.width >= 600;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1024;
+      MediaQuery.of(context).size.width >= 1100;
 
-  /// based on platform
   static bool isDesktopApp() =>
       kIsWeb || Platform.isMacOS || Platform.isWindows || Platform.isLinux;
   static bool isMobileApp() => !kIsWeb || Platform.isIOS || Platform.isAndroid;
@@ -34,17 +32,21 @@ class ResponsiveWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    // desktop
-    if (size.width >= 1024) {
-      return desktop;
+    // If our width is less than 600 then we consider it a mobile
+    if (size.width < 600) {
+      return mobile;
     }
     // If width it less then 1100 and more then 850 we consider it as tablet
-    else if (size.width >= 600 && tablet != null) {
+    else if (size.width <= 882 && kIsWeb) {
+      return mobile;
+    }
+    // If width it less then 1100 and more then 850 we consider it as tablet
+    else if (size.width >= 850 && tablet != null) {
       return tablet!;
     }
-    // Or less then that we called it mobile
+    // Or less then that we called it desktop
     else {
-      return mobile;
+      return desktop;
     }
   }
 }
