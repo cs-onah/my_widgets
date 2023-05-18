8e9eee0e96e6b51ae2d1182a179dedd41483ce3e
Chukwuebuka Onah <revelationjay02@gmail.com>
2023-05-18 18:04:07 +0100

    added scoped navigation & string extension

--- lib/my_widgets.dart
+++ lib/my_widgets.dart
@@ -3,6 +3,7 @@ library my_widgets;
 import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 import 'dart:math';
 import 'dart:io';
 
@@ -10,4 +11,6 @@ part 'src/animations/animated_list.dart';
 part 'src/animations/flip_animation.dart';
 part 'src/animations/slide_opacity_transition.dart';
 part 'src/responsiveness/responsiveness.dart';
+part 'src/navigation/scoped_navigator.dart';
+part 'src/extensions/base_string_extension.dart';
 
